5a4b30bef0abc0918dd691d1c1cf43854d3149d6
rasyidahanun <hanunreswara@gmail.com>
2022-11-06 15:35:13 +0700

    peraturan kost

--- resources/views/portofolio/index.blade.php
+++ resources/views/portofolio/index.blade.php
@@ -32,6 +32,27 @@
                                                                   Kost murah dengan fasilitas lengkap. 
                                                                   Lingkungan aman, nyaman, asri, dan tenang.
       </pre>
+      <br>
+        <h1 style="rasputin" size="25">Peraturan</h1>
+        <br>
+        <pre style="now" size="19">
+        1. Mempunyai Kartu Tanda Penduduk (KTP)/ Kartu Mahasiswa/Kartu Pelajar
+           yang berlaku dan menyerahkan fotocopy kartu tersebut 2 lembar kepada pemilik kost.
+        2. DILARANG membawa tamu laki-laki masuk ke dalam rumah kost.
+        3. DILARANG KERAS melakukan tindakan asusila.
+        4. DILARANG KERAS mengkonsumsi/memakai/menggunakan narkoba, minuman keras, serta barang-barang 
+           berbahaya lainnya.
+        5. Pastikan semua barang-barang berharga penghuni kost dijaga dengan baik oleh pemiliknya masing-masing, 
+           jika terjadi kehilangan bukan menjadi tanggung jawab pemilik kost.
+        6. Tidak diperkenankan saling mengganggu sesama penghuni kost dengan menyalakan/menghidupkan radio,
+            televisi terlalu keras dan berbicara dengan sangat keras (berteriak).
+        7. Penghuni kost wajib menjaga keamanan, kenyamanan, dan ketertiban serta kebersihan rumah kost
+        8. Bagi seluruh penghuni yang membawa teman harap memberitahukan kepada pemilik kost semata-mata untuk menjaga keamanan, 
+           kenyamananan, dan ketertiban bersama.
+        9. Tamu penghuni kost yang menginap harap segera melaporkan kepada pemilik kost.
+       10. Pintu pagar ditutup pukul 22.00 WIB dan apabila ada keperluan mendadak/mendesak harap segera memberitahukan kepada pemilik kost.
+       </pre>
+ 
   </nav>
 
   @include('partials.kamar')
