3da21260c5512c4b2fd3a2d128c9a40e24fd6459
DanaMifkhakunNK <danamifkhakunnurkhasan@gmail.com>
2022-11-07 21:30:23 +0700

    menambah folder icon dan memperbarui contact me

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -9,8 +9,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href={{ asset('fontawesome6.2.0/css/all.min.css') }} />
   </head>
   
     
