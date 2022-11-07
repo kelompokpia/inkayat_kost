7168b85ecd26be412c69233bebc018133efb6b88
BISAKOK <ridhonovan951@gmail.com>
2022-11-07 22:46:24 +0700

    Login Eror Bosass..mumett kiihhh

---
+++ app/Http/Controllers/login.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class login extends Controller
+{
+    function index(){
+        return view("sesi/index");
+    }
+    function index()
+}
