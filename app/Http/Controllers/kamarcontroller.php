fd88f8a4e2d80b971c3a01dd0948803e02d06b6d
BISAKOK <ridhonovan951@gmail.com>
2022-11-14 16:08:45 +0700

    contoller

---
+++ app/Http/Controllers/kamarcontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class kamarcontroller extends Controller
+{
+    public function show()
+{
+    return view('datakamaradmin.data_kamar');
+}
+}
