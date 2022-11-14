024c0fe9589a94851d8ca6180436be8f8058fbfc
DanaMifkhakunNK <danamifkhakunnurkhasan@gmail.com>
2022-11-14 14:14:34 +0700

    instal node js

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Aplikasi Inkayat Kost
 
-dana
+Install Node Js https://nodejs.org/en/download/
+ketikkan satu persatu di terminal vscode
+sesuai urutan
+-composer require laravel/ui
+-php artisan ui bootstrap
+-npm install
+-npm run dev
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Admin Inkayat Kost</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
