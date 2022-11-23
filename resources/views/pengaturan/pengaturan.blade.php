c5a87b092b7ce1725da2ad2b61033a3f88cc204e
DanaMifkhakunNK <danamifkhakunnurkhasan@gmail.com>
2022-11-23 14:40:47 +0700

    frontend login

--- resources/views/pengaturan/pengaturan.blade.php
+++ resources/views/pengaturan/pengaturan.blade.php
@@ -22,17 +22,44 @@
   <div class="container-lg py-5">
     <div class="row">
       <div class="col-lg-10">
-        <h2 class="fw-bold mb-2">Ubah Data Admin</h2>
+        <h2 class="fw-bold mb-2">Data Admin</h2>
       </div>
       <div class="col-lg-2">
-        <a class="text-decoration-none btn btn-dark  justify-content-center" href="{{ route('register') }}">{{ __('Register') }}</a>
+        <a class="text-decoration-none btn btn-dark justify-content-center" href="{{ route('register') }}">{{ __('Register') }}</a>
       </div>
   </div>
   <div class="row justify-content-center">
     <div class="col-lg-12">
       <div class="card d-flex justify-content-center py-5 w-100 align-items-center" style="height: 50vh">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <form action='{{url('mahasiswa/'.$data->nim) }}' method='post'>
+            @csrf
+            @method('PUT')
+            <div class="my-3 p-3 bg-body rounded shadow-sm">
+                <a href='{{ url('mahasiswa') }}' class="btn btn-secondary btn-sm"><< kembali</a>
+                <div class="mb-3 row">
+                    <label for="nim" class="col-sm-2 col-form-label">NIM</label>
+                    <div class="col-sm-10">
+                        {{$data->nim}}
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name='nama' value="{{ $data->nama }}" id="nama">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="jurusan" class="col-sm-2 col-form-label">Jurusan</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name='jurusan' value="{{ $data->jurusan }}" id="jurusan">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="jurusan" class="col-sm-2 col-form-label"></label>
+                    <div class="col-sm-10"><button type="submit" class="btn btn-primary" name="submit">SIMPAN</button></div>
+                </div>
+        </form>
         </div>
       </div>
     </div>
